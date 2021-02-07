0c8e6e2d2a58fe965ea22546117a55c5ddd425bc
gracea18 <72771341+gracea18@users.noreply.github.com>
2021-02-07 15:40:34 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 while True:
-    if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0 or input.rotation(Rotation.ROLL) > 0 or input.rotation(Rotation.ROLL) < 0:
+    if input.rotation(Rotation.PITCH) != 0:
         music.set_volume(255)
         music.siren.play_until_done()
         light.set_all(light.rgb(255,255,255))
+
+    else: 
+        light.clear()
+        music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 while (true) {
-    if (input.rotation(Rotation.Pitch) > 0 || input.rotation(Rotation.Pitch) < 0 || input.rotation(Rotation.Roll) > 0 || input.rotation(Rotation.Roll) < 0) {
+    if (input.rotation(Rotation.Pitch) != 0) {
         music.setVolume(255)
         music.siren.playUntilDone()
         light.setAll(light.rgb(255, 255, 255))
+    } else {
+        light.clear()
+        music.stopAllSounds()
     }
     
 }
